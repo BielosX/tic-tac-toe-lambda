ee03aa7fb9671824262d779d94df3e8a6934d200
Mateusz Biel <matbiel@gmail.com>
2024-06-12 11:21:54 +0200

    Disable ObjectMapper FAIL_ON_UNKNOWN_PROPERTIES

--- lambdas/commons/src/main/java/org/ttt/commons/ObjectMapperFactory.java
+++ lambdas/commons/src/main/java/org/ttt/commons/ObjectMapperFactory.java
@@ -1,5 +1,6 @@
 package org.ttt.commons;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -8,6 +9,7 @@ public class ObjectMapperFactory {
   public static ObjectMapper create() {
     ObjectMapper mapper = new ObjectMapper();
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     mapper.registerModule(new JavaTimeModule());
     return mapper;
   }
