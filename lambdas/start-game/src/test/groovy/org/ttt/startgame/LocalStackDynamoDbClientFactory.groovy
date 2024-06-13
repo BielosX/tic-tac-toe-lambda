e9d90b2ed710d050380992ebfc3f14b5f16b962a
Mateusz Biel <matbiel@gmail.com>
2024-06-13 11:48:02 +0200

    Refactor LocalStackDynamoDbClientFactory

--- lambdas/start-game/src/test/groovy/org/ttt/startgame/LocalStackDynamoDbClientFactory.groovy
+++ lambdas/start-game/src/test/groovy/org/ttt/startgame/LocalStackDynamoDbClientFactory.groovy
@@ -8,9 +8,10 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
 class LocalStackDynamoDbClientFactory {
 	static DynamoDbClient create(LocalStackContainer container) {
+		def awsCredentials = AwsBasicCredentials.create(container.accessKey, container.secretKey)
 		return DynamoDbClient.builder()
 				.region(Region.of(container.region))
 				.endpointOverride(container.endpoint)
-				.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(container.accessKey, container.secretKey))).build()
+				.credentialsProvider(StaticCredentialsProvider.create(awsCredentials)).build()
 	}
 }
