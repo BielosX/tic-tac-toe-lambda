f4dfe937b114bee16d1cf7813c5b24f5df5f1d9b
Mateusz Biel <matbiel@gmail.com>
2024-06-23 11:44:43 +0200

    Add test to GamesServiceSpec

--- lambdas/commons/src/test/groovy/org/ttt/commons/GamesServiceSpec.groovy
+++ lambdas/commons/src/test/groovy/org/ttt/commons/GamesServiceSpec.groovy
@@ -1,11 +1,14 @@
 package org.ttt.commons
 
 import org.ttt.commons.exceptions.GameAlreadyFinishedException
+import org.ttt.commons.exceptions.WrongSymbolException
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import software.amazon.awssdk.services.dynamodb.model.GetItemResponse
 import spock.lang.Shared
 import spock.lang.Specification
+
+import static org.ttt.commons.GameSymbol.NOUGHT
 
 class GamesServiceSpec extends Specification {
 	@Shared
@@ -30,5 +33,27 @@ class GamesServiceSpec extends Specification {
 
 		then:
 		thrown GameAlreadyFinishedException
+	}
+
+	def "should throw exception when wrong symbol is provided"() {
+		setup:
+		def client = Mock(DynamoDbClient)
+		GamesService uat = new GamesService(provider, client)
+		def playerId = UUID.randomUUID().toString()
+		def opponentId = UUID.randomUUID().toString()
+		def gameId = UUID.randomUUID().toString()
+		client.getItem(_) >> GetItemResponse.builder()
+				.item([
+					"state": AttributeValue.fromS("ACTIVE"),
+					"symbolMapping": AttributeValue.fromM([
+						(playerId): AttributeValue.fromS("CROSS"),
+						(opponentId): AttributeValue.fromS("NOUGHT"),
+					])
+				]).build()
+		when:
+		uat.commitMove(playerId, gameId, new CommitMoveRequest(5, 0, 0, NOUGHT))
+
+		then:
+		thrown WrongSymbolException
 	}
 }
