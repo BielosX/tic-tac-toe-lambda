38912818ba81ded3c855f5616dd9c0cefe1231fc
Mateusz Biel <matbiel@gmail.com>
2024-07-03 15:55:21 +0200

    Init frontend

---
+++ frontend/mock/index.js
@@ -0,0 +1,16 @@
+import express, {Router} from 'express'
+
+const app = express()
+const port = 8080
+
+const router = new Router()
+
+router.get('/games', (req, res) => {
+    res.send('Hello')
+})
+
+app.use('/api/v1', router)
+
+app.listen(port, () => {
+    console.log(`Mock app listening on port ${port}`)
+})
