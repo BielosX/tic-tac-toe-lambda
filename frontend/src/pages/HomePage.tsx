de3ca213bf52ae7e99e7e7e020b994bcd68b3561
Mateusz Biel <matbiel@gmail.com>
2024-07-09 14:32:52 +0200

    Add FC type to pages

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Container, Typography } from '@mui/material'
+import {FC} from "react";
 
-export const HomePage = () => {
+export const HomePage: FC = () => {
   return (
     <Container>
       <Typography>
