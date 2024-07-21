d99af1b5c65fae7fafa5dec95926e22341db2b9e
Mateusz Biel <matbiel@gmail.com>
2024-07-21 11:43:19 +0200

    Fix Menu position

--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -32,10 +32,6 @@ export const UserAvatar: FC = () => {
       <Menu
         keepMounted
         anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
